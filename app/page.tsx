d750f46dc2e14a839bb9c7f0496e632b4e985191
Guiler <129989701+GuilhermeSerafim@users.noreply.github.com>
2025-11-10 19:13:20 +0000

    fix: cta

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <HeroSection />
       <FeaturesSection />
       <GeoRiskMap/>
-      <CTASection />
+      {/* <CTASection /> */}
       <Footer />
     </main>
   )
